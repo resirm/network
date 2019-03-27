4ba4b4feba63200546e2bfbc71e7d183efa2394c
Yifeng Zhu <zhuyf95@outlook.com>
2019-03-27 12:23:08 +0800

    add asio daytime demo with cmake

---
+++ C++/IM_demo/client.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <asio.hpp>
+
+using asio::ip::tcp;
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "Usage: client <host>" << std::endl;
+      return 1;
+    }
+
+    asio::io_context io_context;
+
+    tcp::resolver resolver(io_context);
+    tcp::resolver::results_type endpoints =
+      resolver.resolve(argv[1], "daytime");
+
+    tcp::socket socket(io_context);
+    asio::connect(socket, endpoints);
+
+    for (;;)
+    {
+      std::array<char, 128> buf;
+      asio::error_code error;
+
+      size_t len = socket.read_some(asio::buffer(buf), error);
+
+      if (error == asio::error::eof)
+        break; // Connection closed cleanly by peer.
+      else if (error)
+        throw asio::system_error(error); // Some other error.
+
+      std::cout.write(buf.data(), len);
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
